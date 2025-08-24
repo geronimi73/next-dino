529d3d63a071191efb1b775aa1643aeb81a11c18
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-08-24 18:20:35 +0200

    halfway there

--- app/page.jsx
+++ app/page.jsx
@@ -7,8 +7,9 @@ import { Upload, X, ImageIcon, Loader2, CheckCircle, AlertCircle, RotateCcw, Squ
 import { cn } from "@/lib/utils"
 
 // transformer.js stuff
-import { pipeline, RawImage, matmul } from "@huggingface/transformers"
+import { pipeline, RawImage, matmul, env } from "@huggingface/transformers"
 const MODEL_ID = "onnx-community/dinov3-vits16-pretrain-lvd1689m-ONNX";
+const PATCH_SIZE = 16
 const EXAMPLE_IMAGE_URL = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/cats.png";
 
 const DEFAULT_FILE = "/samples/image_05.jpg"
@@ -22,23 +23,88 @@ export default function HomePage() {
   const [busy, setBusy] = useState(false)
   const [file, setFile] = useState(null)
   const [imageReady, setImageReady] = useState(false)
+  const [patchSimScores, setPatchSimScores] = useState(false)
 
+  const offscreenCanvasRef = useRef(null)
   const canvasRef = useRef(null)
+  const extractorRef = useRef(null)
+
+  // process image 
+  async function processImage() {
+    setBusy(true)
+    setStatus("Processing image ..")
+
+    const extractor = extractorRef.current
+
+    const imageData = await RawImage.fromCanvas(canvasRef.current);
+    const features = await extractor(imageData);
+    const numRegisterTokens = extractor.model.config.num_register_tokens ?? 0;
+    const startIndex = 1 + numRegisterTokens;
+    const patchFeatures = features.slice(null, [startIndex, null]);
+    const normalizedFeatures = patchFeatures.normalize(2, -1);
+    const scores = await matmul(normalizedFeatures, normalizedFeatures.permute(0, 2, 1));
+    const similarityScores = (await scores.tolist())[0];
+    setPatchSimScores(similarityScores)
+
+    setBusy(false)
+    setStatus(`Done (${similarityScores.length} patches). Move around`)
+  }
+
+  // redraw canvas from offscreen canvas (=original image)
+  function redrawCanvas() {
+    const offscreenCanvas = offscreenCanvasRef.current
+    const onscreenCanvas = canvasRef.current
+
+    const ctx = onscreenCanvas.getContext("2d")
+    ctx.drawImage(
+      offscreenCanvas, 
+      0, 0, offscreenCanvas.width, offscreenCanvas.height,
+      0, 0, onscreenCanvas.width, onscreenCanvas.height
+    )
+  }
 
   // draw chosen image file onto canvas
-  async function drawImageFile() {
+  async function drawImage() {
     // load image
     const img = new Image()
     img.crossOrigin = "anonymous"
     img.src = URL.createObjectURL(file)
     await img.decode()
 
-    // draw image onto canvas
+    // resize if necessary
+    const isMobile = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    );
+    const maxPixels = isMobile ? 1048576 : 2097152;
+    const currPixels = img.naturalWidth * img.naturalHeight
+    let newDim = {
+      width: img.naturalWidth,
+      height: img.naturalHeight,
+    }
+    if (currPixels > maxPixels) {
+      const scaleF = Math.sqrt(maxPixels / currPixels)
+      newDim.width = Math.floor(scaleF * newDim.width)
+      newDim.height = Math.floor(scaleF * newDim.height)
+
+      console.log(`${img.naturalWidth}x${img.naturalHeight} image resized to ${newDim.width}x${newDim.height}`)
+    }
+
+    // draw original image onto offscreen screnvas
+    const offscreenCanvas = document.createElement('canvas');
+    const offscreenCtx = offscreenCanvas.getContext("2d")
+    offscreenCanvas.width = newDim.width
+    offscreenCanvas.height = newDim.height
+    offscreenCtx.drawImage(img, 
+      0, 0, img.naturalWidth, img.naturalHeight,
+      0, 0, offscreenCanvas.width, offscreenCanvas.height
+    )
+    offscreenCanvasRef.current = offscreenCanvas
+
+    // setup and draw onscreen canvas
     const canvas = canvasRef.current
-    canvas.width = img.naturalWidth
-    canvas.height = img.naturalHeight
-    const ctx = canvas.getContext("2d")
-    ctx.drawImage(img, 0, 0)
+    canvas.width = newDim.width
+    canvas.height = newDim.height
+    redrawCanvas()
 
     setImageReady(true)
   }
@@ -50,15 +116,17 @@ export default function HomePage() {
     try {
       const extractor = await pipeline("image-feature-extraction", MODEL_ID,
        {
-        "device": "wasm",
+        "device": "webgpu",
         "dtype": "q4",
       });
+      extractorRef.current = extractor
       extractor.processor.image_processor.do_resize = false;
       const patchSize = extractor.model.config.patch_size;
       const device = extractor.model.sessions.model.config.device
       const dtype = extractor.model.sessions.model.config.dtype
 
       setStatus(`Model ready on device ${device} (${dtype}) patch size ${patchSize}. Select an image.`);
+      setModelReady(true)
     } catch (error) {
       setStatus("Failed to load the model. Please refresh.");
       console.error("Model loading error:", error);
@@ -73,18 +141,74 @@ export default function HomePage() {
     setFile(imageFile)
   }
 
+  function updateHeatmap(mousePos) {
+    redrawCanvas()
 
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext("2d")
+
+    const patchesPerRow = canvas.width / PATCH_SIZE
+    const currPatchIdx = Math.floor(mousePos.y / PATCH_SIZE) * patchesPerRow + Math.floor(mousePos.x / PATCH_SIZE);
+
+    const currPatchCoords = {
+      x: (currPatchIdx % patchesPerRow) * PATCH_SIZE,
+      y: Math.floor(currPatchIdx / patchesPerRow) * PATCH_SIZE,
+    }
+
+    ctx.globalAlpha = 0.1; // 50% transparency
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(currPatchCoords.x, currPatchCoords.y, PATCH_SIZE, PATCH_SIZE);
+    ctx.globalAlpha = 1.0; 
+  }
+
+  function resetUI() {
+    setFile(null)
+    setImageReady(false)
+  }
+
+  // model+image ready -> process
+  useEffect(() => {
+    if (modelReady && imageReady) {
+      // disable for now
+      // processImage()
+    }
+  }, [modelReady, imageReady]); 
+
+  // file is read -> draw image onto canvas
   useEffect(() => {
     if (file && canvasRef.current) {
-      drawImageFile()
+      drawImage()
     }
   }, [file]); 
 
+  // load model on page load
   useEffect(() => {
     if (!modelReady) {
       loadModel()
     }
   }, []); 
+
+  // attach mousemove handler to canvas
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    const handleMouseMove = (e) => {
+      const rect = canvas.getBoundingClientRect()
+      const mousePos = {
+        x: (e.clientX - rect.left) * (canvas.width / rect.width),
+        y: (e.clientY - rect.top) * (canvas.height / rect.height)
+      }
+
+      requestAnimationFrame(() => {updateHeatmap(mousePos)})
+    }
+
+    canvas.addEventListener('mousemove', handleMouseMove)
+    
+    return () => {
+      canvas.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [imageReady]) // Re-attach when image changes
 
   const handleDrag = useCallback((e) => {
     e.preventDefault()
@@ -175,7 +299,7 @@ export default function HomePage() {
                       </Button>
                       <Button  className="mt-4">
                         <Upload className="w-4 h-4 mr-2" />
-                        Choose Files
+                        Choose File
                       </Button>
                     </div>
                   </div>
@@ -189,7 +313,7 @@ export default function HomePage() {
                 <div className="flex items-center justify-between mb-4">
                   <h3 className="text-lg font-semibold text-foreground">Your Image</h3>
                   <Button variant="outline" 
-                    onClick={()=>{setFile(null)}}
+                    onClick={resetUI}
                   >
                     <RotateCcw className="w-4 h-4 mr-2" />
                     Reset
@@ -205,7 +329,6 @@ export default function HomePage() {
                 </div>
               </CardContent>
             </Card>
-
           )
         }
       </div>
